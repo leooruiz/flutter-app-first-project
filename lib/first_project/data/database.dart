58da3f67692f7aaaec18b57843d94876527e4be8
Leonardo <leoruizz14@outlook.com>
2023-09-25 17:21:57 -0300

    adding database files

---
+++ lib/first_project/data/database.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_studies/first_project/data/task_dao.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+Future<Database> getDataBase() async {
+  final String path = join(await getDatabasesPath(), 'task.db');
+  return openDatabase(
+    path,
+    onCreate: (db, version) {
+      db.execute(TaskDao.tableSql);
+    },
+    version: 1,
+  );
+}
+
+
